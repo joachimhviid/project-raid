2aaefd5755240df913d6a3b828d9984951fc7e9f
joachimhviid <joac1099@gmail.com>
2021-11-08 09:42:27 +0100

    Init repo

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header>This is a default layout</header>
+    <slot />
+    <footer>Footer ph</footer>
+  </div>
+</template>
+<script lang="ts">
+import '@/assets/tailwind.scss'
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="font-bold text-red-600">index page</div>
+</template>
+<script lang="ts">
+export default defineComponent({
+  data() {
+    return {
+      test: 'hello'
+    }
+  },
+  mounted() {
+    // console.log('hello')
+  }
+})
+</script>
