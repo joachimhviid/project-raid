76a018273e9bb0821a97b8e5c66eecb47e66b032
joachimhviid <joac1099@gmail.com>
2021-12-23 19:16:29 +0100

    Responsive guild page

--- components/Dashboard/Card/GuildInfo.vue
+++ components/Dashboard/Card/GuildInfo.vue
@@ -1,40 +1,36 @@
 <template>
-  <div class="bg-white dark:bg-rblue dark:bg-opacity-70 rounded-md p-4">
-    <div class="flex">
-      <div class="flex-col text-center space-y-2 mx-6 pr-9 pt-5">
-        <span class="text-rblue dark:text-white font-rtext text-3xl font-bold">Guildo Buildo</span>
-        <div
-          class="
-            items-center
-            w-44
-            h-44
-            overflow-hidden
-            rounded-full
-            border-4 border-rblue
-            dark:border-white
-            self-center
-          "
-        >
-          <img class="h-full w-full object-cover" src="@/assets/img/guild_icon.jpeg" alt="" />
-        </div>
-        <span class="text-rblue dark:text-white opacity-80 font-rtext text-3xl tracking-wide">62 Members</span>
-      </div>
-
-      <div class="flex-col max-w-2xl space-y-4 mt-2">
-        <span
-          class="text-rblue dark:text-white font-rtext text-3xl tracking-wide underline decoration-rblue decoration-1"
-        >
-          Description
-        </span>
-        <p class="text-rblue dark:text-white font-rtext text-xl tracking-wide font-thin max overflow-hidden">
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking
-          at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-          opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
-          packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'
-          will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).
-        </p>
-      </div>
+  <div
+    class="
+      bg-white
+      dark:bg-rblue dark:bg-opacity-70
+      rounded-lg
+      p-4
+      flex flex-col
+      md:flex-row
+      gap-4
+      md:gap-8
+      justify-between
+    "
+  >
+    <div class="flex flex-col items-center text-center flex-shrink-0">
+      <h2 class="text-rblue dark:text-white font-rtext text-3xl font-bold">Guildo Buildo</h2>
+      <img
+        class="items-center w-28 h-28 overflow-hidden rounded-full border-4 border-rblue dark:border-white"
+        src="@/assets/img/guild_icon.jpeg"
+        alt="Guild icon"
+      />
+      <span class="text-rblue dark:text-white opacity-80 font-rtext text-2xl">62 Members</span>
+    </div>
+    <div class="flex flex-col">
+      <h2 class="text-rblue dark:text-white font-rtext text-2xl font-bold">Description</h2>
+      <p class="text-rblue dark:text-white font-rtext">
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at
+        its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing
+        packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'
+        will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </p>
     </div>
   </div>
 </template>
--- components/Dashboard/Card/GuildMembers.vue
+++ components/Dashboard/Card/GuildMembers.vue
@@ -1,348 +1,44 @@
 <template>
-  <div class="bg-white bg-opacity-80 dark:bg-rblue dark:bg-opacity-70 rounded-md cursor-default">
-    <div class="flex flex-col mx-8 my-3 max-w-5xl space-y-4 cursor-default">
-      <span class="text-rblue dark:text-white font-rtext text-3xl tracking-wide underline decoration-rblue decoration-1"
-        >Members</span
-      >
+  <div class="flex flex-col bg-white bg-opacity-80 dark:bg-rblue dark:bg-opacity-70 rounded-lg p-4">
+    <h2 class="text-rblue dark:text-white font-rtext font-bold text-2xl mb-4">Members</h2>
 
-      <div class="flex flex-wrap gap-x-6 gap-y-5 overflow-auto max-h-64">
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
+    <div class="grid grid-cols-2 gap-4 overflow-y-scroll max-h-64">
+      <div v-for="i in 10" :key="i" class="flex bg-rblue dark:bg-white w-full h-28 rounded-md items-center">
+        <div class="relative w-28 h-28 rounded-lg">
+          <img
+            src="@/assets/img/sylv.jpg"
+            alt=""
+            class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
+          />
+          <span
+            class="
+              text-white
+              dark:text-rblue
+              font-rtext
+              text-center text-sm
+              dark:font-medium
+              absolute
+              w-2/3
+              rounded-t-md
+              bottom-0
+              bg-rblue
+              dark:bg-white
+            "
+            >Level 60
+          </span>
         </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
-        </div>
-
-        <div class="flex bg-rblue dark:bg-white w-72 h-24 rounded-md space-x-2 items-center">
-          <div class="relative w-28 h-24 rounded-md overflow-hidden">
-            <img
-              src="@/assets/img/sylv.jpg"
-              alt=""
-              class="w-full h-full object-cover border-rblue dark:border-white border-4 rounded-lg"
-            />
-            <span
-              class="
-                text-white
-                dark:text-rblue
-                font-rtext
-                text-center text-sm
-                dark:font-medium
-                absolute
-                w-2/3
-                rounded-t-md
-                bottom-0
-                bg-rblue
-                dark:bg-white
-              "
-              >Level 60</span
-            >
-          </div>
-          <div class="flex flex-col">
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Sylvanas</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Undead Hunter</span
-            >
-            <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
-              >Kharazan EU</span
-            >
-          </div>
+        <div class="flex flex-col">
+          <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
+            >Sylvanas</span
+          >
+          <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
+            >Undead Hunter</span
+          >
+          <span class="text-white dark:text-rblue font-rtext text-xl tracking-wide font-light dark:font-medium"
+            >Kharazan EU</span
+          >
         </div>
       </div>
     </div>
   </div>
 </template>
-<script lang="ts">
-export default defineComponent({})
-</script>
-<style lang="scss" scoped></style>
