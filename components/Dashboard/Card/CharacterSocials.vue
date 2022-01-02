e240f87a920eeb362297a01d6207704242a82d81
joachimhviid <joac1099@gmail.com>
2022-01-02 16:13:45 +0100

    Turns out CI was broken

--- components/Dashboard/Card/CharacterSocials.vue
+++ components/Dashboard/Card/CharacterSocials.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="bg-white dark:bg-rblue dark:bg-opacity-70 rounded-lg py-6 px-8 space-y-7">
     <button class="flex items-center space-x-4">
-      <img class="w-12" src="./assets/icons/twitch_small.png" alt="twitch logo" />
+      <img class="w-12" src="~/assets/icons/twitch_small.png" alt="twitch logo" />
       <span class="text-rblue dark:text-white font-rtext text-2xl">SylvTTV</span>
     </button>
     <button class="flex items-center space-x-4">
-      <img class="w-12" src="./assets/icons/twitter_small.png" alt="twitter logo" />
+      <img class="w-12" src="~/assets/icons/twitter_small.png" alt="twitter logo" />
       <span class="text-rblue dark:text-white font-rtext text-2xl">SylvanasKhara</span>
     </button>
     <button class="flex items-center space-x-4">
-      <img class="w-12" src="./assets/icons/youtube_small.png" alt="youtube logo" />
+      <img class="w-12" src="~/assets/icons/youtube_small.png" alt="youtube logo" />
       <span class="text-rblue dark:text-white font-rtext text-2xl">SylvanasKharaU</span>
     </button>
     <button class="flex items-center space-x-4">
-      <img class="w-12" src="./assets/icons/discord_small.png" alt="discord logo" />
+      <img class="w-12" src="~/assets/icons/discord_small.png" alt="discord logo" />
       <span class="text-rblue dark:text-white font-rtext text-2xl">discord.gg/Sylva</span>
     </button>
   </div>
